a940c52ee3df84c0f5c47fec329619730771df78
AhmedAmirSalem <ahmad_amir@live.com>
2026-03-10 04:19:50 +0200

    chore: configure turborepo and fix app structure

---
+++ apps/nextjs-crash-course/lib/constants.ts
@@ -0,0 +1,59 @@
+export type EventItem = {
+  title: string;
+  image: string;
+  slug: string;
+  location: string;
+  date: string;
+  time: string;
+};
+
+export const events: EventItem[] = [
+  {
+    image: "/images/event1.png",
+    title: "Event 1",
+    slug: "event-1",
+    location: "location-1",
+    date: "2025-11-07",
+    time: "8:00 PM",
+  },
+  {
+    image: "/images/event2.png",
+    title: "Event 2",
+    slug: "event-2",
+    location: "location-2",
+    date: "2026-11-07",
+    time: "10:00 AM",
+  },
+  {
+    image: "/images/event3.png",
+    title: "Event 3",
+    slug: "event-3",
+    location: "location-3",
+    date: "2026-11-07",
+    time: "10:00 AM",
+  },
+  {
+    image: "/images/event4.png",
+    title: "Event 4",
+    slug: "event-4",
+    location: "location-4",
+    date: "2026-11-07",
+    time: "10:00 AM",
+  },
+  {
+    image: "/images/event5.png",
+    title: "Event 5",
+    slug: "event-5",
+    location: "location-5",
+    date: "2026-11-07",
+    time: "10:00 AM",
+  },
+  {
+    image: "/images/event6.png",
+    title: "Event 6",
+    slug: "event-6",
+    location: "location-6",
+    date: "2026-11-07",
+    time: "10:00 AM",
+  },
+];
